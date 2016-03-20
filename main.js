0c29c96cd3792dcb5fc7c7f17cddd4ebdc31bb9d
Atul Varma <varmaa@gmail.com>
2016-03-20 19:58:44 -0400

    link shaders into a program.

--- main.js
+++ main.js
@@ -28,16 +28,30 @@ function buildShader(gl, shaderType, source) {
     throw new Error(shaderType + " failed to compile: " +
                     gl.getShaderInfoLog(shader));
   }
+
+  return shader;
 }
 
 Promise.all([
   readFile('vertex.glsl'),
   readFile('fragment.glsl')
 ]).then(function(values) {
+  var program = gl.createProgram();
   var vertexShader = buildShader(gl, 'VERTEX_SHADER', values[0]);
   var fragmentShader = buildShader(gl, 'FRAGMENT_SHADER', values[1]);
 
   console.log("Compiled shaders.");
+
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    throw new Error("Failed to link shader program: " +
+                    gl.getProgramInfoLog(program));
+  }
+
+  console.log("Linked shader program.");
 }).catch(function(reason) {
   console.log("Failed to fetch and compile shaders!", reason);
 });
