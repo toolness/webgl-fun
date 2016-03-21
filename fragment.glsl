f6e92d845bfecf819b46ef3d2d195f8d055a7949
Atul Varma <varmaa@gmail.com>
2016-03-21 18:00:26 -0400

    linearly interpolate colors via a varying.

--- fragment.glsl
+++ fragment.glsl
@@ -1,5 +1,9 @@
 // I am a fragment shader.
 
+precision mediump float;
+
+varying vec4 v_color;
+
 void main(void) {
-  gl_FragColor = vec4(0, 1.0, 0, 1.0);
+  gl_FragColor = v_color;
 }
--- vertex.glsl
+++ vertex.glsl
@@ -2,6 +2,9 @@
 
 attribute vec2 a_position;
 
+varying vec4 v_color;
+
 void main(void) {
   gl_Position = vec4(a_position, 0, 1);
+  v_color = gl_Position * 0.5 + 0.5;
 }
